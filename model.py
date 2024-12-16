4092fd5da20d57a43bfeacca746143a5cbed8f6b
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2024-12-16 17:38:53 +0000

    Add files via upload

--- model.py
+++ model.py
@@ -70,9 +70,9 @@ while running:
     if keys[pygame.K_RIGHT]:
         x += 1
     if keys[pygame.K_UP]:
-        z = min(z_max, z + 1)
+        z = min(z_max, z + 5)
     if keys[pygame.K_DOWN]:
-        z = max(z_min, z - 1)
+        z = max(z_min, z - 5)
     if x<-(z):
         x=-(z)
     if x>z:
