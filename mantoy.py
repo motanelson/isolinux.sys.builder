bf8bf187d95c5f2b61aebeaca71883164ff3c0ee
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2024-12-16 12:06:17 +0000

    Add files via upload

--- mantoy.py
+++ mantoy.py
@@ -25,9 +25,9 @@ boneco_original_height = boneco_image.get_height()
 
 # Posição inicial do boneco
 x = 0
-z = 10  # Controle da "profundidade"
-z_min = 5
-z_max = 50
+z = 100  # Controle da "profundidade"
+z_min = 50
+z_max = 500
 scale_factor = 10
 
 # Loop principal
@@ -48,13 +48,17 @@ while running:
         z = max(z_min, z - 1)
     if keys[pygame.K_DOWN]:
         z = min(z_max, z + 1)
+    if x<-(z*0.1):
+        x=-(z*0.1)
+    if x>(z*0.1):
+        x=(z*0.1)
 
     # Calcular parâmetros com base em z
-    scale = 1 / z * scale_factor
+    scale = 1 / (z*0.1) * scale_factor
     scaled_width = int(boneco_original_width * scale)
     scaled_height = int(boneco_original_height * scale)
     center_x = screen_width // 2 + int(x * scale * 20)
-    center_y = screen_height // 2 + int(z * scale * 10)
+    center_y = screen_height // 2 + int((z*0.1) * scale * 10)
 
     # Redimensionar a imagem do boneco
     boneco_scaled = pygame.transform.scale(boneco_image, (scaled_width, scaled_height))
